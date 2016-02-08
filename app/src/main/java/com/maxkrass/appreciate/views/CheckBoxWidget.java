47d4a07e6542e5851a0b3c9b51f936bed583684a
maxkrass <maxkrass.com@gmail.com>
2016-02-08 01:03:35 +0100

    modified CheckBoxWidget

--- app/src/main/java/com/maxkrass/appreciate/views/CheckBoxWidget.java
+++ app/src/main/java/com/maxkrass/appreciate/views/CheckBoxWidget.java
@@ -1,70 +1,73 @@
 package com.maxkrass.appreciate.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.maxkrass.appreciate.R;
 
-public class CheckBoxWidget extends FrameLayout
-{
+public class CheckBoxWidget extends FrameLayout implements Checkable {
 
     private CheckBox checkBox;
     private TextView titleView;
 
-    public CheckBoxWidget(Context context)
-    {
+    public CheckBoxWidget(Context context) {
         super(context);
-        init((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+        init((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE), false, "");
     }
 
-    public CheckBoxWidget(Context context, AttributeSet attributeset)
-    {
-        super(context, attributeset);
-        init((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+    public CheckBoxWidget(Context context, AttributeSet attributeset) {
+        this(context, attributeset, 0);
     }
 
-    public CheckBoxWidget(Context context, AttributeSet attributeset, int i)
-    {
+    public CheckBoxWidget(Context context, AttributeSet attributeset, int i) {
         super(context, attributeset, i);
-        init((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE));
+        boolean checked;
+        String text;
+        TypedArray a = context.getTheme().obtainStyledAttributes(attributeset, R.styleable.CheckBoxWidget, 0, 0);
+        try {
+            checked = a.getBoolean(R.styleable.CheckBoxWidget_checked, false);
+            text = a.getString(R.styleable.CheckBoxWidget_label);
+        } finally {
+            a.recycle();
+        }
+        init((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE), checked, text);
     }
 
-    private void init(LayoutInflater layoutinflater)
-    {
+    private void init(LayoutInflater layoutinflater, boolean checked, String text) {
         layoutinflater.inflate(R.layout.checkbox_widget, this);
-        titleView = (TextView)findViewById(R.id.title);
-        checkBox = (CheckBox)findViewById(R.id.checkbox);
+        titleView = (TextView) findViewById(R.id.title);
+        setTitleView(text);
+        checkBox = (CheckBox) findViewById(R.id.checkbox);
+        setChecked(checked);
     }
 
     public void toggle() {
-        checkBox.setChecked(!checkBox.isChecked());
+        checkBox.toggle();
     }
 
-    public boolean isChecked()
-    {
+    public boolean isChecked() {
         return checkBox.isChecked();
     }
 
-    public void setCheckBox(boolean flag)
-    {
+    public void setChecked(boolean flag) {
         checkBox.setChecked(flag);
     }
 
-    public void setTitleView(String s)
-    {
+    public void setTitleView(String s) {
         titleView.setText(s);
     }
 
-	public void setEnabled(boolean enabled) {
-		checkBox.setEnabled(enabled);
-	}
+    public void setEnabled(boolean enabled) {
+        checkBox.setEnabled(enabled);
+    }
 
-    public String toString()
-    {
+    public String toString() {
         return String.valueOf(isChecked()) + "";
     }
 }
